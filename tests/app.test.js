f519d95aa4e6130cac522449e99c470bf435049f
Emily <ryokaa77@gmail.com>
2025-10-05 12:19:03 +0800

    Refactor app test to use server for requests
    Updated test to use server instance instead of app.

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,19 @@
 const request = require('supertest');
-const app = require('../app'); 
+const app = require('../app');
+
+let server;
+
+beforeAll(() => {
+  server = app.listen(8080); 
+});
+
+afterAll((done) => {
+  server.close(done); 
+});
 
 describe('GET /', () => {
   it('should return 200 OK', async () => {
-    const res = await request(app).get('/');
+    const res = await request(server).get('/'); // 传入 server 而不是 app
     expect(res.statusCode).toBe(200);
   });
 });
