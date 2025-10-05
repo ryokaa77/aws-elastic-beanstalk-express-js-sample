3c708a8230f8a1266cbb7aed4b57b7b0aadf35e0
Emily <ryokaa77@gmail.com>
2025-10-05 12:26:15 +0800

    Refactor test to use app instance instead of URL

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,9 @@
 const request = require('supertest');
+const app = require('../app');
 
 describe('GET /', () => {
   it('should return 200 OK', async () => {
-    const res = await request('http://localhost:8080').get('/'); // 直接访问本地端口
+    const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
   });
 });
